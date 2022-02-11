d6af27187dd52ee6e1263e43c08fcb4a964cd4e0
Bubba <rob@stechstudio.com>
2022-02-10 22:37:52 -0500

    Exorcism

---
+++ src/bin/daemon.ts
@@ -0,0 +1,26 @@
+import { NS } from "Bitburner";
+import { openPorts } from "/lib/Ports.js";
+
+const weaken = "/bin/hack/weaken.js";
+const grow = "/bin/hack/grow.js";
+const hack = "/bin/hack/hack.js";
+const share = "/bin/hack/share.js";
+const ONE_MINUTE = 60000;
+const files = [weaken, grow, hack];
+
+const singularityFunctionsAvailable = true;
+const backdoorScript = "/bin/backdoor.js"
+const backdoorScriptRam = 65.8;
+
+let jobs = [
+    {
+        script: `${backdoorScript} faction`,
+        delay: ONE_MINUTE * 20,
+        lastrun: performance.now()
+    },
+];
+
+export async function main(ns: NS) {
+
+
+}
