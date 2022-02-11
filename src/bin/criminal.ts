fbd5a5a97bca63ca4788431a6c20b7576424c777
Bubba <rob@stechstudio.com>
2022-02-10 22:37:20 -0500

    The Criminal

---
+++ src/bin/criminal.ts
@@ -0,0 +1,15 @@
+import { NS } from "Bitburner";
+import { Crimes } from "/lib/Crime.js";
+
+export async function main(ns: NS) {
+    ns.disableLog("ALL");
+    ns.tail();
+    let timeout = 250;
+    let crimes: Crimes = new Crimes(ns);
+
+    while (true) {
+        await ns.sleep(timeout);
+        if (ns.isBusy()) continue;
+        crimes.commitBestCrime();
+    }
+}
