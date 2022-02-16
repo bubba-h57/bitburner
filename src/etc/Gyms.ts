c76109e34da089d8d22541d215a1748e9dc96ba8
Bubba <rob@stechstudio.com>
2022-02-15 19:20:36 -0500

    Gym configurations

---
+++ src/etc/Gyms.ts
@@ -0,0 +1,26 @@
+export const config = {
+  locations: [
+    { city: 'Aevum', gym: 'Snap Fitness Gym' },
+    { city: 'Sector-12', gym: 'Powerhouse Gym' },
+    { city: 'Volhaven', gym: 'Millenium Fitness Gym' },
+  ],
+
+  levels: {
+    initial: {
+      strength: 100,
+      dexterity: 100,
+      defense: 100,
+      agility: 100,
+    },
+    advance: {
+      strength: 400,
+      dexterity: 400,
+      defense: 400,
+      agility: 400,
+    },
+  },
+  training: {
+    interval: 60000,
+  },
+  statistics: ['strength', 'agility', 'defense', 'dexterity'],
+};
